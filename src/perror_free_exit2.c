5460ef2292b14eeea2451dbcc87dfc24de0de18e
talessantos49 <talessantos49@gmail.com>
2023-08-16 16:31:51 -0300

    WIP: Norminette no perror free

--- src/perror_free_exit2.c
+++ src/perror_free_exit2.c
@@ -6,17 +6,31 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 20:06:08 by root              #+#    #+#             */
-/*   Updated: 2023/08/15 20:19:42 by root             ###   ########.fr       */
+/*   Updated: 2023/08/16 16:29:52 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+void	free_around(t_block *current_cmd)
+{
+	t_cmd	*next_cmd;
+	t_cmd	*current;
+
+	next_cmd = ft_calloc(1, sizeof(t_cmd));
+	current = current_cmd->commands;
+	while (current)
+	{
+		next_cmd = current->next;
+		safe_free(&current);
+		current = next_cmd;
+	}
+}
+
 void	free_pipe_list(t_shell **shell, t_block *current)
 {
 	t_block	*next;
 	t_cmd	*current_cmd;
-	t_cmd	*next_cmd;
 
 	if (current)
 		current_cmd = current->commands;
@@ -30,15 +44,42 @@ void	free_pipe_list(t_shell **shell, t_block *current)
 		safe_free(&current->args);
 		unlink(current->heredoc_name);
 		safe_free(&(*shell)->heredoc_name);
-		while (current_cmd)
-		{
-			next_cmd = current_cmd->next;
-			safe_free(&current_cmd);
-			current_cmd = next_cmd;
-		}
+		if (current_cmd)
+			free_around(current);
 		next = current->next;
 		safe_free(&current);
 		(*shell)->pipelist = NULL;
 		current = next;
 	}
 }
+
+// void	free_pipe_list(t_shell **shell, t_block *current)
+// {
+// 	t_block	*next;
+// 	t_cmd	*current_cmd;
+// 	t_cmd	*next_cmd;
+
+// 	if (current)
+// 		current_cmd = current->commands;
+// 	else
+// 		return ;
+// 	if (current_cmd && current->cmd != current_cmd->arg)
+// 		safe_free(&current->cmd);
+// 	while (current)
+// 	{
+// 		free_args_matrix(current, current->args);
+// 		safe_free(&current->args);
+// 		unlink(current->heredoc_name);
+// 		safe_free(&(*shell)->heredoc_name);
+// 		while (current_cmd)
+// 		{
+// 			next_cmd = current_cmd->next;
+// 			safe_free(&current_cmd);
+// 			current_cmd = next_cmd;
+// 		}
+// 		next = current->next;
+// 		safe_free(&current);
+// 		(*shell)->pipelist = NULL;
+// 		current = next;
+// 	}
+// }
