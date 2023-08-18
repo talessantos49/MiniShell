63bab7877e403d4288d4cc00008387642c9e0f24
talessantos49 <talessantos49@gmail.com>
2023-08-18 14:16:38 -0300

    WIP: Subida para o workspace

--- src/exit.c
+++ src/exit.c
@@ -6,53 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 20:01:15 by root              #+#    #+#             */
-/*   Updated: 2023/08/18 12:18:59 by root             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-// #include "../inc/minishell.h"
-
-// int	is_number(char *str)
-// {
-// 	str += (str[0] == CHAR_MINUS || str[0] == CHAR_PLUS);
-// 	if (*str)
-// 	{
-// 		while (*str && ft_isdigit(*str))
-// 			str++;
-// 		if (!*str)
-// 			return (TRUE);
-// 	}
-// 	return (FALSE);
-// }
-
-
-// void	c_exit(t_shell **shell)
-// {
-// 	int8_t	exit_code;
-
-// 	if ((*shell)->pipelist->args[1] 
-// 	&& !is_number((*shell)->pipelist->args[1]))
-// 	{
-// 		perror((*shell)->pipelist->args[1]);
-// 		(*shell)->exit_code = 2;
-// 	}
-// 	else if ((*shell)->pipelist->args[1])
-// 		(*shell)->exit_code = ft_atoi((*shell)->pipelist->args[1]);
-// 	exit_code = (*shell)->exit_code;
-// 	free_shell(shell);
-// 	exit(exit_code);
-// }
-
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   exit.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/15 20:01:15 by root              #+#    #+#             */
-/*   Updated: 2023/08/18 01:21:27 by root             ###   ########.fr       */
+/*   Updated: 2023/08/18 14:02:27 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,7 +65,7 @@ void	c_exit(t_shell **shell)
 		exit_code = 1;
 	}
 	else
-		(*shell)->exit_code = 0;
+		(*shell)->exit_code = 2;
 	free_shell(shell);
 	exit(exit_code);
 	return ;
